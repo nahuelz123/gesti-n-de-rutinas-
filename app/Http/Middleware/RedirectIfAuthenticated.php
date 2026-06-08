9dd43007baa7fc343b5d020008894312de3a2cd3
Nahuel Zamudio <84518078+nahuelz123@users.noreply.github.com>
2026-06-08 16:35:49 -0300

    Create RedirectIfAuthenticated.php

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    public function handle(Request $request, Closure $next, string ...$guards)
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                $user = Auth::guard($guard)->user();
+                if ($user->role === 'client') {
+                    return redirect()->route('client.dashboard');
+                }
+                return redirect('/admin');
+            }
+        }
+
+        return $next($request);
+    }
+}
