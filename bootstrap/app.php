d92368b874960513d19cebbfff924aea702f2894
Nahuel Zamudio <84518078+nahuelz123@users.noreply.github.com>
2026-06-08 16:20:56 -0300

    Add PreventBackNavigation middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
 ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->alias([
-        'filament.access' => \App\Http\Middleware\EnsureNotClientForFilament::class,
-        'client' => \App\Http\Middleware\EnsureClientRole::class,
-    ]);
+   $middleware->alias([
+    'filament.access' => \App\Http\Middleware\EnsureNotClientForFilament::class,
+    'client' => \App\Http\Middleware\EnsureClientRole::class,
+    'no-back' => \App\Http\Middleware\PreventBackNavigation::class,
+]);
 })
 ->withExceptions(function (Exceptions $exceptions): void {
     //
