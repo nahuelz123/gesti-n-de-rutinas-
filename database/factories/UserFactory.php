4cb92dc38e36753b47338dc862f6d84ef18a5a46
Nahuel Zamudio <84518078+nahuelz123@users.noreply.github.com>
2026-09-24 12:58:53 -0300

    Give generated test users a gym

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Gym;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,6 +25,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'gym_id' => fn () => Gym::create(['name' => fake()->company()])->id,
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
